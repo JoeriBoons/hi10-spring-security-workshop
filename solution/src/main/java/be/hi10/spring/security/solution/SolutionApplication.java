be1c416ff6e8cccd1283de7d42e6a7576dac4830
Joeri Boons <joeri.boons@ravago.com>
2020-01-14 12:35:07 +0100

    Initial commit, with 2 empty projects

---
+++ solution/src/main/java/be/hi10/spring/security/solution/SolutionApplication.java
@@ -0,0 +1,12 @@
+package be.hi10.spring.security.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolutionApplication.class, args);
+    }
+}
---
+++ workshop/src/main/java/be/hi10/spring/security/workshop/WorkshopApplication.java
@@ -0,0 +1,12 @@
+package be.hi10.spring.security.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopApplication.class, args);
+    }
+}
