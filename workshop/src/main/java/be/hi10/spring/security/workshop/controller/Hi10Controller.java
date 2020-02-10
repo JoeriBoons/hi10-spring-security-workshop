4bfc915b89511cf8ea9949f128ba05d1067617fa
Joeri Boons <joeri.boons@hi10.be>
2020-02-10 22:03:35 +0100

    Add previous powerpoint

--- workshop/src/main/java/be/hi10/spring/security/workshop/controller/Hi10Controller.java
+++ workshop/src/main/java/be/hi10/spring/security/workshop/controller/Hi10Controller.java
@@ -2,12 +2,7 @@ package be.hi10.spring.security.workshop.controller;
 
 public class Hi10Controller {
 
-    /**
-     * Step 3 Add a getMapping method 'secured'
-     */
+    //Step 3 Add a getMapping method 'secured'
 
-
-    /**
-     * Step 5 Add a getMapping method 'admin' only
-     */
+    //Step 5 Add a getMapping method 'admin' only
 }
